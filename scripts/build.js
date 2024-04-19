6a5f1bb64f97a62a155bad18422d8ac9c348af52
NOZK <andre.marques@academiadecodigo.org>
2024-04-19 22:02:37 +0100

    First draft for build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,43 @@
 import { build } from 'esbuild';
-import process from 'node:process';
 import path from 'path';
 import { BUILD_DIR } from './commons/commons.js';
+
+await build({
+    entryPoints: ['css/*.css'],
+    bundle: true,
+    minify: true,
+    external: ['/*'],
+    loader: {
+        '.png': 'copy',
+        '.jpg': 'copy',
+        '.svg': 'dataurl'
+    },
+    outdir: `${path.join(BUILD_DIR, '/css')}`
+});
+
+await build({
+    entryPoints: ['index.html', 'favicon/*'],
+    loader: {
+        '.ico': 'copy',
+        '.png': 'copy',
+        '.html': 'copy',
+        '.svg': 'copy',
+        '.webmanifest': 'copy',
+        '.xml': 'copy'
+    },
+    outdir: `${path.resolve(BUILD_DIR)}`
+});
+
+await build({
+    entryPoints: ['js/app.js'],
+    format: 'esm',
+    bundle: true,
+    minify: true,
+    splitting: true,
+    treeShaking: true,
+    loader: { '.html': 'text' },
+    drop: ['debugger', 'console'],
+    dropLabels: ['DEV'],
+    logLevel: 'info',
+    outdir: `${path.join(BUILD_DIR, '/js')}`
+});
