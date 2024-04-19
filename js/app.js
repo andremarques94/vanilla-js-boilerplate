116127d4361828556e5cf693933db1e7935fbc74
NOZK <andre.marques@academiadecodigo.org>
2024-04-19 21:49:48 +0100

    Starts the implementation of the build script

--- js/app.js
+++ js/app.js
@@ -3,13 +3,14 @@ DEV: new EventSource('/esbuild').addEventListener('change', () => window.locatio
 window.addEventListener('DOMContentLoaded', () => {
     const app = document.querySelector('#app');
     app.style = 'display:flex;justify-content:center;align-items:center;';
-    app.innerHTML = `<div style="text-align: center;"><h1>BOILERPLATE FOR VANILLA-JS</h1>
-       <br>
-      <h2>USING ESBUILD</h2>
-       <br>
+    app.innerHTML = `<div style="text-align: center;">
+        <h1>BOILERPLATE FOR VANILLA-JS</h1>
         <br>
-      <h2>To get started, edit <code>js/app.js</code> and save to reload.</h2>
-       <br>
-      <h2>Happy coding!</h2>
+        <h2>USING ESBUILD</h2>
+        <br>
+        <br>
+        <h2>To get started, edit <code>js/app.js</code> and save to reload.</h2>
+        <br>
+        <h2>Happy coding!</h2>
       </div>`;
 });
---
+++ scripts/build.js
@@ -0,0 +1,4 @@
+import { build } from 'esbuild';
+import process from 'node:process';
+import path from 'path';
+import { BUILD_DIR } from './commons/commons.js';
